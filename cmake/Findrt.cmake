19c6fa7afdbaf7475f9778a225e3f2217caf205b
errata-c <erratachem@gmail.com>
2021-10-12 12:27:28 -0400

    Attmpt fix rt module

--- cmake/Findrt.cmake
+++ cmake/Findrt.cmake
@@ -7,33 +7,26 @@
 #  rt_DEFINITIONS - Compiler switches required for using rt
 #
 # Also creates an import target called rt::rt
-
-find_path (rt_INCLUDE_DIR NAMES time.h
-  PATHS
-  /usr
-  /usr/local
-  /opt
-  PATH_SUFFIXES
-)
-
-find_library(rt_LIBRARIES NAMES rt
-  PATHS
-  /usr
-  /usr/local
-  /opt
-)
-
-include(FindPackageHandleStandardArgs)
-
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(rt DEFAULT_MSG rt_LIBRARIES rt_INCLUDE_DIR)
-
-mark_as_advanced(rt_INCLUDE_DIR rt_LIBRARIES)
-
 if (NOT TARGET rt::rt)
-  add_library(rt::rt INTERFACE IMPORTED)
+	find_path (rt_INCLUDE_DIR NAMES time.h
+	  PATHS
+	  /usr
+	  /usr/local
+	  /opt
+	  PATH_SUFFIXES
+	)
 
-  set_target_properties(rt::rt PROPERTIES
-    INTERFACE_INCLUDE_DIRECTORIES ${rt_INCLUDE_DIR}
-    INTERFACE_LINK_LIBRARIES ${rt_LIBRARIES}
-  )
+	find_library(rt_LIBRARIES NAMES rt
+	  PATHS
+	  /usr
+	  /usr/local
+	  /opt
+	)
+	
+	add_library(rt::rt INTERFACE IMPORTED)
+
+	set_target_properties(rt::rt PROPERTIES
+	INTERFACE_INCLUDE_DIRECTORIES ${rt_INCLUDE_DIR}
+	INTERFACE_LINK_LIBRARIES ${rt_LIBRARIES}
+	)
 endif()
